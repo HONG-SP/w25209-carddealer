e1d93011e9b17ec9e07f1f67eca5c7365f96ed14
HONG-SP <hong359352@naver.com>
2025-11-10 15:44:22 +0900

    📦️Controller

---
+++ src/main/kotlin/kr/ac/kumoh/s20241290/w25209_carddealer/controller/CardController.kt
@@ -0,0 +1,14 @@
+package kr.ac.kumoh.s20241290.w25209_carddealer.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class CardController {
+    @GetMapping("/cards/random")
+    fun getRandomCards(model: Model): String {
+        model.addAttribute("cards", "ace_of_spades.png")
+        return "cards"
+    }
+}
